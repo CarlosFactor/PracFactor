80777364d6069c92667bcc913bdcdf5266a5bbb1
Carlos <carlos.delvalle@factorlibre.com>
2021-04-30 12:59:58 +0000

    Record_Store solucioado problema al introducir reserva y precio total

--- record_store/models/record_reservation_line.py
+++ record_store/models/record_reservation_line.py
@@ -10,7 +10,7 @@ class RecordReservationLine(models.Model):
 
     name_reservation_line = fields.Char("Description", required=True,related="record.title")
     record = fields.Many2one("record.record", "Record")
-    artist_id = fields.Many2many("record.artist", "Artist")
+    artist_id = fields.Many2one("record.artist", "Artist")
     price = fields.Float("Price Record", related="record.price")
     reservation_unit = fields.Integer("Reserved Units", default = "1")
     client = fields.Many2one("res.partner", "Client")
@@ -22,9 +22,9 @@ class RecordReservationLine(models.Model):
             record.total_price += record.price * record.reservation_unit
 
     # Esto podemos quitarlo por el uso del related en los campos 
-    @api.onchange("record")
-    def _onchange_record(self):
-        if self.record:
-            # self.name_reservation_line = self.record.title
-            self.artist_id = self.record.artist_ids
-            # self.price = self.record.price
+    # @api.onchange("record")
+    # def _onchange_record(self):
+    #     if self.record:
+    #         # self.name_reservation_line = self.record.title
+    #         self.artist_id = self.record.artist_ids
+    #         # self.price = self.record.price
